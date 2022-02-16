5e3659404cbd7570a2390863a81b031093676f2a
roadrunnerrr1 <96847032+roadrunnerrr1@users.noreply.github.com>
2022-02-15 18:05:40 -0800

    Added my own config and personalization

--- bookmarks.js
+++ bookmarks.js
@@ -3,45 +3,18 @@ const bookmarks = [
   {
     title: "Daily",
     links: [
-      { name: "Inbox", url: "https://inbox.google.com" },
+      { name: "AUR", url: "https://aur.archlinux.org"},
       { name: "GitHub", url: "https://github.com" },
-      { name: "Drive", url: "https://drive.google.com" },
+      { name: "Docs", url: "https://docs.google.com" },
     ],
   },
+
   {
     title: "Media",
     links: [
-      { name: "Youtube", url: "https://youtube.com" },
-      { name: "Netflix", url: "https://netflix.com" },
-      { name: "Crunchyroll", url: "https://crunchyroll.com" },
-      {
-        name: "Amazon Prime",
-        url: "https://www.amazon.com/Amazon-Video",
-      },
-    ],
-  },
-  {
-    title: "Reddit",
-    links: [
-      { name: "/r/overwatch", url: "https://reddit.com/r/overwatch" },
-      {
-        name: "/r/pcmasterrace",
-        url: "https://reddit.com/r/pcmasterrace",
-      },
-      { name: "/r/me_irl", url: "https://reddit.com/r/me_irl" },
-      {
-        name: "/r/battlestations",
-        url: "https://reddit.com/r/battlestations",
-      },
-      { name: "/r/unixporn", url: "https://reddit.com/r/unixporn" },
-      { name: "/r/news", url: "https://reddit.com/r/news" },
-    ],
-  },
-  {
-    title: "Social",
-    links: [
       { name: "Twitter", url: "https://twitter.com" },
-      { name: "Facebook", url: "https://facebook.com" },
+      { name: "Youtube", url: "https://youtube.com" },
+      { name: "Spotify", url: "https://spotify.com" },
     ],
   },
 ];
